d53c6492f2ce7aa729949a5c16de678191e759e8
senRsl <dong_M@yeah.net>
2018-10-29 18:59:13 +0800

    rev1

---
+++ clock/src/main/java/dc/test/xposed/TestActivity.java
@@ -0,0 +1,12 @@
+package dc.test.xposed;
+
+import android.app.Activity;
+
+/**
+ * @author senrsl
+ * @ClassName: TestActivity
+ * @Package: dc.test.xposed.clock
+ * @CreateTime: 2018/10/29 下午6:42
+ */
+public class TestActivity extends Activity {
+}
---
+++ clock/src/main/java/dc/test/xposed/clock/TestClock.java
@@ -0,0 +1,34 @@
+package dc.test.xposed.clock;
+
+import android.graphics.Color;
+import android.widget.TextView;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+/**
+ * @author senrsl
+ * @ClassName: TestClock
+ * @Package: dc.test.xposed.clock
+ * @CreateTime: 2018/10/29 下午4:48
+ */
+public class TestClock implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        if (!loadPackageParam.packageName.equals("com.android.systemui"))
+            return;
+
+        findAndHookMethod("com.android.systemui.statusbar.policy.Clock", loadPackageParam.classLoader, "updateClock", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                TextView tv = (TextView) param.thisObject;
+                String text = tv.getText().toString();
+                tv.setText(text + " :)");
+                tv.setTextColor(Color.RED);
+            }
+        });
+    }
+}
